a0dd054ffd0324d4d10c41748ff0313baf77581f
jscheidt <jscheidt@rams.colostate.edu>
2016-04-23 16:15:48 -0600

    Added two mappers and two reducers for DB dates
    DBUserDateMapper/Reducer gets the first and last date that each user made for each DB.
    DBLastPostMapper/Reducer gets the global min and max date for each DB, regardless of user.
    
    Added my Main method as well.

---
+++ DBLastPostMapper.java
@@ -0,0 +1,24 @@
+
+package gp;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class DBLastPostMapper extends Mapper<Object, Text, Text, Text> {
+
+	// this reads in from the file created in DBUserDateMapper/Reducer
+	//the format is "DB<===>USER \t minVal:maxVal"
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+		String[] line = value.toString().split("\t");
+		String[] keyPart = line[0].split("<==>");
+		String database = keyPart[0].trim();
+//		String user = keyPart[1].trim();
+		String vals =	line[1].trim();
+		System.out.println(database + "and " + vals);
+		context.write(new Text(database), new Text(vals));
+		}
+	
+}
