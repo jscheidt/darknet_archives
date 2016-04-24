23bd1487b090a8232944371c8adde30f6b8d05f1
jscheidt <jscheidt@rams.colostate.edu>
2016-04-24 16:45:39 -0600

    Update DBUserDateMapper.java
    added a check that the data is formatted correctly. also check if there any non-numeric values in timestamp and removes them if there are.

--- userDate/DBUserDateMapper.java
+++ userDate/DBUserDateMapper.java
@@ -15,14 +15,16 @@ public class DBUserDateMapper extends Mapper<Object, Text, Text, IntWritable>{
 
 	    IntWritable intW;
 		String[] line = value.toString().split("\",\"");
+		if( line.length < 4)
+			return;
 		String database = line[0].trim();
 		database = database.replace("\"", "");
 		String user = line[1].trim();
 //		String description = line[2].trim();
 		String timestamp = line[3].trim();
 		timestamp = timestamp.replace("\",", "");
+		timestamp = timestamp.replaceAll("[^0-9.]", "");
 		// System.out.println(timestamp);
-//		StringTokenizer words = new StringTokenizer(description);
 		try{
 			int timestampInt = Integer.parseInt(timestamp);
 			intW = new IntWritable(timestampInt);
