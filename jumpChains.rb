c40772ebd93af1f07c72ef116fe151ab269e9fb1
Greg Boyarko <boyarkogc@gmail.com>
2016-04-29 11:03:33 -0600

    jumpChains script

--- jumpChains.rb
+++ jumpChains.rb
@@ -1,13 +1,29 @@
+=begin
+Script to filter top matches. Users that have activity under 10000 are discarded....
+Script not finished atm
+=end
+
+#####################################################
+
+marketDates = Hash.new
+input = File.open('userDate/userDateGlobal', "r")
+
+input.readlines.each do |line|
+	tabSplit = line.strip.split("\t")
+	marketDates[tabSplit[0]] = tabSplit[1]
+end
+
+input.close
+
+#####################################################
 lines = Array.new
 
-#output = File.open('uniqueChains5000r1', "w+")
-#input = File.open('run1-5000', "r")
-#output = File.open('sortedMergedChains', "w+")
-#input = File.open('simactivity/sortedMerged', "r")
 output = File.open('finalSortedDataJumps.out', 'w+')
 input = File.open('finalSortedData1','r')
-#output = File.open('uniqueChains', "w+")
-#input = File.open('top', "r")
+#output = File.open('jumps', "w+")
+#input = File.open('simactivity/sortedMerged', "r")
+#output = File.open('jumps5000r2', "w+")
+#input = File.open('run2-5000', "r")
 
 input.readlines.each do |line|
     lines.push(line)
@@ -22,19 +38,30 @@ index = 0
 
 for l in lines
     while index < lines.size && lines.size > 0
-        tabSplit = lines[index].split("\t")
-        cosineSplit = tabSplit[1].split("<====>")
-        userSplit = cosineSplit[1].split("<=0=>")
-        user1 = userSplit[0].gsub(/"/, '').strip
-        user2 = userSplit[1].gsub(/"/, '').strip
-        market1 = user1.split("<==>")[0]
-        market2 = user2.split("<==>")[0]
-        cosine = cosineSplit[0].gsub(/"/, '').strip
-        match = cosineSplit[1].gsub(/"/, '').strip
+	split = l.gsub(/"/, '').strip.split(/\t|<=0=>|<====>|<===>|<==>|<=A=>|<F=L>/)
+	cosine = split[1]
+	market1 = split[2]
+	user1 = l.gsub(/"/, '').strip.split(/\t|<=0=>|<====>/)[2]
+	activity1 = split[4]
+	firstDiff1 = split[5]
+	firstPost1 = marketDates[market1].split(":")[0].to_i + firstDiff1.to_i
+	lastDiff1 = split[6]
+	lastPost1 = marketDates[market1].split(":")[1].to_i - lastDiff1.to_i
+	market2 = split[7]
+	user2 = l.gsub(/"/, '').strip.split(/\t|<=0=>|<====>/)[3]
+	activity2 = split[9]
+	firstDiff2 = split[10]
+	firstPost2 = marketDates[market2].split(":")[0].to_i + firstDiff2.to_i
+	lastDiff2 = split[11]
+	lastPost2 = marketDates[market2].split(":")[1].to_i - lastDiff2.to_i
 
-        if groupCosines.empty?
+	if (lastDiff2.to_i > 17000000 and lastDiff1.to_i > 17000000) or activity1.to_i < 4250000 or activity2.to_i < 4250000
+		#this is to filter out users who did not post their last post on one market and their first on another within ~4 weeks OR
+		#have had less than ~1 week of activity on market
+		lines.delete_at(index)
+	elsif groupCosines.empty?
             unless allElements.include?(user1) or allElements.include?(user2)
-                groupCosines[match] = cosine
+                groupCosines["#{user1}<=0=>#{user2}"] = cosine
                 allElements.push(user1)
                 allElements.push(user2)
                 groupElements.push(user1)
